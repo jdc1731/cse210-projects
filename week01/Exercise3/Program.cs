7fcb0e555389f75c8fdc0b709104b53916f7829e
jdc1731 <jkeil@byupathway.edu>
2025-07-08 18:23:20 -0600

    exercise 3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 101);
+
+        
+        int guess;
+
+    do
+        {
+            Console.Write("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (guess != magicNumber);
+     Console.WriteLine("You guessed it!");
+        
+
     }
 }
