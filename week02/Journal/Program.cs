727619f363babe1d29d54d79157141533feec86a
jdc1731 <jkeil@byupathway.edu>
2025-07-18 22:24:51 -0600

    fingers crossed

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -13,7 +13,7 @@ class Program
         {
             Console.WriteLine("Please select one of the following choices:");
             Console.WriteLine("1. Write");
-            Console.WriteLine("2. Display");.
+            Console.WriteLine("2. Display");
             Console.WriteLine("3. Save");
             Console.WriteLine("4. Load");
             Console.WriteLine("5. Quit");
