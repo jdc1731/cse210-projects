628485d5103c13f901198f00e3d9e1a23c0435de
jdc1731 <jkeil@byupathway.edu>
2025-07-27 21:03:57 -0600

    finished

---
+++ week03/Fractions/fraction.cs
@@ -0,0 +1,44 @@
+public class Fractions
+{
+    private int numerator;
+    private int denominator;
+    public Fractions()
+    {
+        numerator = 1;
+        denominator = 1;
+    }
+    public Fractions(int top)
+    {
+        numerator = top;
+        denominator = 1;
+    }
+    public Fractions(int top, int bottom)
+    {
+        numerator = top;
+        denominator = bottom;
+    }
+    public int GetNumerator()
+    {
+        return numerator;
+    }
+    public int GetDenominator()
+    {
+        return denominator;
+    }
+    public void SetNumerator(int top)
+    {
+        numerator = top;
+    }
+    public void SetDenominator(int bottom)
+    {
+        denominator = bottom;
+    }
+   public string GetFractionString()
+    {
+        return $"{numerator}/{denominator}";
+    }
+    public double GetDecimalValue()
+    {
+        return (double)numerator / denominator;
+    }
+}
