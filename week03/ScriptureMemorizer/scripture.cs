43f4a67bd909f667d2c2a86ebdc0bbf658e1c635
jdc1731 <jkeil@byupathway.edu>
2025-07-29 19:23:17 -0600

    scripture finished

--- week03/ScriptureMemorizer/scripture.cs
+++ week03/ScriptureMemorizer/scripture.cs
@@ -1,4 +1,48 @@
-public class Scripture;
+using System.Runtime.CompilerServices;
+
+public class Scripture
 {
-    
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+        string[] wordArray = text.Split(' ');
+        foreach (string word in wordArray)
+        {
+            _words.Add(new Word(word));
+        } 
+    }
+    public void HideRandomWords(int count)
+    {
+        Random random = new Random();
+        int hiddenCount = 0;
+
+        while (hiddenCount < count && _words.Any(w => !w.IsHidden()))
+        {
+            int index = random.Next(_words.Count);
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string displayText = _reference.GetDisplayText() + " ";
+        foreach (Word word in _words)
+        {
+            displayText += word.GetDisplayText() + " ";
+        }
+        return displayText.Trim();
+    }
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+
 }
