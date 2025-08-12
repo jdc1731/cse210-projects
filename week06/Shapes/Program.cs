e835847f1e3e3e8225c4d6a8fe217911f2a648d3
jdc1731 <jkeil@byupathway.edu>
2025-08-11 22:17:58 -0600

    finished

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using Shapes;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square(5, "Red");
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle(4, 6, "Blue");
+        shapes.Add(s2);
+
+        Circle s3 = new Circle(3, "Green");
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"Shape Color: {color}, Area: {area}");
+        }
+
     }
 }
